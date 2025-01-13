170cebbccb8d46818b65a02bec1e3a3f146bc5fc
Patrick <patrick.ramos.motta@gmail.com>
2025-01-12 22:58:08 -0300

    feat: add documentação automatica com swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from '@modules/app.module'
 import 'dotenv/config'
 import { ValidationPipe } from '@nestjs/common'
 import { useContainer } from 'class-validator'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { projectName } from '@common/utils/projectName'
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule)
@@ -16,6 +18,15 @@ async function bootstrap(): Promise<void> {
 	)
 
 	useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+	const documentBuilderConfig = new DocumentBuilder()
+		.setTitle(projectName)
+		.setVersion('1.0')
+		.build()
+	const document = SwaggerModule.createDocument(app, documentBuilderConfig)
+
+	SwaggerModule.setup('/swagger', app, document)
+
 	await app.listen(process.env.SERVICE_PORT ?? 3000)
 }
 bootstrap()
--- src/modules/v1/user/models/dto/updateUser.dto.ts
+++ src/modules/v1/user/models/dto/updateUser.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types'
+import { PartialType } from '@nestjs/swagger'
 import { UserCreateDto } from '@modules/v1/user/models/dto/userCreate.dto'
 
 export class UpdateUserDto extends PartialType(UserCreateDto) {}
